de2b71031d1b0593e1be6d0348243d79dff73d77
Pocket-Deer <58855174+Pocket-Deer@users.noreply.github.com>
2024-09-24 13:40:15 +0700

    Create sthelper.user.js

---
+++ sthelper.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         STHelper
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Testing
+// @author       Pocket Deer
+// @match        https://*steam-trader.com*
+// @include      https://*steam-trader.com*
+// @updateURL    https://raw.githubusercontent.com/Pocket-Deer/STHelper/main/sthelper.user.js
+// @downloadURL  https://raw.githubusercontent.com/Pocket-Deer/STHelper/main/sthelper.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Hello World');
+})();
